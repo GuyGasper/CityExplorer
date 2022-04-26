bd911227651fa67ba04c638450e4feeba86dab6f
My MacPro <gasperg@umich.edu>
2022-04-25 23:59:23 -0500

    Making the inital XCode project

---
+++ CityExplorer/CityExplorer/CityExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityExplorerApp.swift
+//  CityExplorer
+//
+//  Created by Guy Gasper on 4/25/22.
+//
+
+import SwiftUI
+
+@main
+struct CityExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MapView()
+        }
+    }
+}
---
+++ CityExplorer/CityExplorer/MapScreen/MapView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CityExplorer
+//
+//  Created by Guy Gasper on 4/25/22.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+            .previewDevice("iPhone 11")
+    }
+}
